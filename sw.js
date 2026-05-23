3f63ab14e03a0f62bfcfecd4824627249af3f591
mariusbudiun-droid <mariusbudiun@gmail.com>
2026-05-23 16:55:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Giada Companion - Service Worker
    Per forzare aggiornamento: incrementa CACHE_VERSION */
 'use strict';
-const CACHE_VERSION = 'giada-v1.0.1';
+const CACHE_VERSION = 'giada-v1.0.2';
 const ASSETS = [
   './',
   './index.html',
